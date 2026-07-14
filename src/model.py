9006a7b6777532a2b0c6b5f1209df9f148645350
arthur1509 <131341323+Arthur-Fernandes-6@users.noreply.github.com>
2026-07-13 21:35:36 -0300

    função para a cnn

--- src/model.py
+++ src/model.py
@@ -1,11 +1,35 @@
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
 
-modelo = keras.Sequential([
-    layers.Dense(64, activation="relu"),
-    layers.Dense(32, activation="relu"),
-    layers.Dense(16, activation="relu"),
-    layers.Dense(1, activation="sigmoid")
-])
+def criar_modelo():
+    modelo = keras.Sequential([
+        # encontra os padrões
+        Conv2D(
+            filters =32, kernel_size = (3, 3), 
+            activation = 'relu', 
+            input_shape = (224, 224, 3)
+            ),
+        MaxPooling2D(pool_size = (2,2)),
+        #mantem os padrões mais importantes
 
-print(modelo)
+        # Aprende padrões mais complexos
+        Conv2D(
+            filters=64, 
+            kernel_size=(3, 3), 
+            activation='relu'), 
+
+        MaxPooling2D(pool_size=(2, 2)),
+        
+        # transforma  a matriz da imagem em um array
+        Flatten(),
+        
+
+        # Analisa as características encontradas
+        Dense(128, activation= 'relu'),
+        Dense(1, activation='sigmoid')
+    ])
+
+    return modelo
