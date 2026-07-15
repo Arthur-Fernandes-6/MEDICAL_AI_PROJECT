0b9e7524950ae62224013ad60edc0db40079feb0
arthur1509 <131341323+Arthur-Fernandes-6@users.noreply.github.com>
2026-07-15 15:08:17 -0300

    predict terminado

--- src/preditc.py
+++ src/preditc.py
@@ -11,3 +11,22 @@ def predict(caminho):
 
     if imagem is None:
         return None
+
+    imagem = np.expand_dims(imagem, axis=0)# cria a nova dimensão no início para adicionar uma imagem.
+
+    resultado = modelo.predict(imagem)
+
+    probabilidade = float(resultado[0][0])
+
+    if probabilidade >= 0.5:
+        classificacao = "Tumor"
+    
+    else:
+        classificacao =  "Sem tumor"
+
+    return classificacao, probabilidade
+
+
+classificacao, probabilidade = predict("datasets/yes/Y1.jpg")
+print("Resultado: ", classificacao)
+print(f"Probabilidade de tumor: {probabilidade * 100:.2f}%")
