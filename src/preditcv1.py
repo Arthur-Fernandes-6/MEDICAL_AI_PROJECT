d19c29e4c2eb76948d28053b3b79c816351ab3f0
arthur1509 <131341323+Arthur-Fernandes-6@users.noreply.github.com>
2026-07-24 19:45:47 -0300

    Update preditcv1.py

--- src/preditcv1.py
+++ src/preditcv1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tensorflow.keras.models import load_model
-from preprocessv1 import preprocessar_imagem
+from preprocessv2 import preprocessar_imagem
 
 
 modelo = load_model("models/brain_tumor_cnn.keras")
